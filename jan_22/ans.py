bd46fe827f915d8e9e2bfdcc87150d3f1e42b2ca
SaikrishnaBommedi <153886886+SaikrishnaBommedi@users.noreply.github.com>
2024-01-23 14:14:52 +0530

    Add files via upload

---
+++ jan_22/ans.py
@@ -0,0 +1,11 @@
+name=""
+password = ""
+
+while name != "aman" or password != 12345:
+    name= input("enter your name : ")
+    password = int(input("Enter your password: "))
+
+print("hello aman thank you for verification")
+
+
+
